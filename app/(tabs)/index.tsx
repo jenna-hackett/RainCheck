b99fe1f3534911fdd0822ca50d6db564285ffb43
Jenna Hackett <jenna.hackett96@gmail.com>
2026-03-25 10:13:56 -0600

    just some comments above screens as a reminder.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,3 +1,12 @@
+/*
+Screens should contain almost no logic.
+They should only:
+- call hooks
+- render components
+- pass props
+--- Everything else (API, storage, formatting) lives in src/.
+*/
+
 import { Text, View } from "react-native";
 
 export default function Index() {
---
+++ app/about.tsx
@@ -0,0 +1,8 @@
+/*
+Screens should contain almost no logic.
+They should only:
+- call hooks
+- render components
+- pass props
+--- Everything else (API, storage, formatting) lives in src/.
+*/
