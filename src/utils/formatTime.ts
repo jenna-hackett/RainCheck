e296335cc4362e62c04f68ed464798cb00d99601
Verity <verity.boyd@edu.sait.ca>
2026-03-25 14:35:49 -0600

    format time helper function added

--- src/utils/formatTime.ts
+++ src/utils/formatTime.ts
@@ -2,3 +2,20 @@
 Functions with no side effects.
 formatTime(clockFormat, date)
 */
+export function formatTime(dateString: string, clockFormat: "12h" | "24h") {
+  const date = new Date(dateString);
+
+  if (clockFormat === "24h") {
+    return date.toLocaleTimeString("en-CA", {
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: false,
+    });
+  }
+
+  return date.toLocaleTimeString("en-US", {
+    hour: "numeric",
+    minute: "2-digit",
+    hour12: true,
+  });
+}
